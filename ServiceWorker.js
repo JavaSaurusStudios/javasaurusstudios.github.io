d3771e583e843539f6644fa6e34b05073d2e1d41
JavaSaurusStudios <98191221+JavaSaurusStudios@users.noreply.github.com>
2025-04-10 14:08:55 +0200

    Removed screenshake during charging

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "JavaSaurus Studios-DeepFrame 9-1.0";
+const contentToCache = [
+    "Build/Web6.loader.js",
+    "Build/Web6.framework.js",
+    "Build/Web6.data",
+    "Build/Web6.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
